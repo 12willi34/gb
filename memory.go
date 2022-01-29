0aa2468b1fa6c5a3a505fbe8ffedc51bc7e00758
12willi34 <williweisssenborn@gmail.com>
2022-01-29 13:49:38 +0100

    8 bit memory funktioniert

--- memory.go
+++ memory.go
@@ -5,7 +5,8 @@ package gb
 //http://gameboy.mongenel.com/dmg/asmmemmap.html
 
 import (
-	"strconv"
+	"strconv";
+	//"fmt";
 )
 
 type memoryunit struct {
@@ -21,23 +22,23 @@ func NewMemoryUnit() memoryunit {
 	return mmu
 }
 
-func (mmu memoryunit) finished_bios() bool {
+func (mmu memoryunit) Finished_bios() bool {
 	return mmu.addr[0xFF50] == 1
 }
 
-func (mmu memoryunit) read_8(i uint16) uint8 {
-	if(!mmu.finished_bios() && i <= 0xFF) {
+func (mmu memoryunit) Read_8(i uint16) uint8 {
+	if(!mmu.Finished_bios() && i <= 0xFF) {
 		return mmu.bios[i]
 	}
 	return mmu.addr[i]
 }
 
-func (mmu memoryunit) write_8(i uint16, data uint8) {
+func (mmu memoryunit) Write_8(i uint16, data uint8) {
 	mmu.addr[i] = data
 }
 
-func (mmu memoryunit) read_16(i uint16) uint16 {
-	if(!mmu.finished_bios() && i <= 0xFF) {
+func (mmu memoryunit) Read_16(i uint16) uint16 {
+	if(!mmu.Finished_bios() && i <= 0xFF) {
 		var left string = strconv.FormatInt(int64(mmu.bios[i]), 2)
 		var right string = strconv.FormatInt(int64(mmu.bios[i + 1]), 2)
 		res, _ := strconv.ParseInt(left + right, 2, 16)
@@ -49,7 +50,7 @@ func (mmu memoryunit) read_16(i uint16) uint16 {
 	return uint16(res)
 }
 
-func (mmu memoryunit) write_16(i uint16, data uint16) {
+func (mmu memoryunit) Write_16(i uint16, data uint16) {
 	mmu.addr[i] = uint8((0) | (data >> 8))
 	mmu.addr[i + 1] = uint8(data & uint16(0xFF))
 }
--- run/main.go
+++ run/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"fmt";
 	"gb"
 )
 
 func main() {
-	gb.NewMemoryUnit()
+	test_memory()
+}
+
+func test_memory() {
+	var key_8 uint16 = 0x1000
+	var val_8 uint8 = 32
+	var key_16 uint16 = 0x1000 + 2
+	var val_16 uint16 = 512
+
+	mmu := gb.NewMemoryUnit()
+	mmu.Write_8(key_8, val_8)
+	fmt.Println("read/write 8 bit addr:", mmu.Read_8(key_8) == val_8)
+
+	mmu = gb.NewMemoryUnit()
+	mmu.Write_16(key_16, val_16)
+	fmt.Println("read/write 16 bit addr:", mmu.Read_16(key_16) == val_16)
 }
