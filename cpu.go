1713281f067f565fa1709d52982f542d7c3e5fdc
12willi34 <williweisssenborn@gmail.com>
2022-01-29 14:35:52 +0100

    load bios in memory

--- cpu.go
+++ cpu.go
@@ -1,7 +1,20 @@
 package gb
 
-import (
-)
+import ()
+
+type registers struct {
+	a uint8
+	b uint8
+	c uint8
+	d uint8
+	e uint8
+	f uint8
+	g uint8
+	h uint8
+	flags uint8
+	sp uint16
+	pc uint16
+}
 
 type cpu struct {}
 
