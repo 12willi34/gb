6449044a80c0f9ebc98b970338b896e34682b3c8
12willi34 <williweisssenborn@gmail.com>
2022-02-22 23:07:48 +0100

    main loop aufgeräumt

--- gameboy.go
+++ gameboy.go
@@ -38,22 +38,10 @@ func (this *GameBoy) Init() {
 
 func (this *GameBoy) loop() {
   for true {
-    vblank := blank_cycles
-    //end := time.Now().UnixMilli() + (1/60)*int64(time.Millisecond)
-    for(vblank > 0) {
-      steps := (*(*this).Processor).Step()
-      if(steps == -1) {
-        return
-      }
-      (*(*this).gpu).Step(steps)
-      (*(*this).timer).Timing(steps)
-      (*(*this).interrupter).handle()
-      vblank -= steps
-    }
-    /*
-    for(time.Now().UnixMilli() < end) {
-      time.Sleep(1*time.Millisecond)
-    }
-    */
+    steps := (*(*this).Processor).Step()
+    if(steps == -1) { return }
+    (*(*this).gpu).Step(steps)
+    (*(*this).timer).Timing(steps)
+    (*(*this).interrupter).handle()
   }
 }
