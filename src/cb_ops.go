67ed58be2c43b9b65c2a5776aa7598cd4aa70b63
12willi34 <williweisssenborn@gmail.com>
2022-04-05 21:53:35 +0200

    mehr cb ops + tetris startet weiter

--- src/cb_ops.go
+++ src/cb_ops.go
@@ -45,18 +45,208 @@ func SWAP_HL(this *cpu) int {
   return 16
 }
 
-func BIT_6_B(this *cpu) int {
-  this.bit(6, (*this).bc.r_high())
+func BIT_7_B(this *cpu) int {
+  this.bit(7, this.bc.r_high())
+  return 8
+}
+
+func BIT_7_C(this *cpu) int {
+  this.bit(7, this.bc.r_low())
+  return 8
+}
+
+func BIT_7_D(this *cpu) int {
+  this.bit(7, this.de.r_high())
+  return 8
+}
+
+func BIT_7_E(this *cpu) int {
+  this.bit(7, this.de.r_low())
   return 8
 }
 
 func BIT_7_H(this *cpu) int {
-  this.bit(7, (*this).hl.r_high())
+  this.bit(7, this.hl.r_high())
   return 8
+}
+
+func BIT_7_L(this *cpu) int {
+  this.bit(7, this.hl.r_low())
+  return 8
+}
+
+func BIT_7_HL(this *cpu) int {
+  this.bit(7, this.mu.Read_8(this.hl.value))
+  return 16
 }
 
 func BIT_7_A(this *cpu) int {
   this.bit(7, this.af.r_high())
+  return 8
+}
+
+func BIT_3_B(this *cpu) int {
+  this.bit(3, this.bc.r_high())
+  return 8
+}
+
+func BIT_3_C(this *cpu) int {
+  this.bit(3, this.bc.r_low())
+  return 8
+}
+
+func BIT_3_D(this *cpu) int {
+  this.bit(3, this.de.r_high())
+  return 8
+}
+
+func BIT_3_E(this *cpu) int {
+  this.bit(3, this.de.r_low())
+  return 8
+}
+
+func BIT_3_H(this *cpu) int {
+  this.bit(3, this.hl.r_high())
+  return 8
+}
+
+func BIT_3_L(this *cpu) int {
+  this.bit(3, this.hl.r_low())
+  return 8
+}
+
+func BIT_3_HL(this *cpu) int {
+  this.bit(3, this.mu.Read_8(this.hl.value))
+  return 16
+}
+
+func BIT_3_A(this *cpu) int {
+  this.bit(3, this.af.r_high())
+  return 8
+}
+
+func BIT_5_B(this *cpu) int {
+  this.bit(5, this.bc.r_high())
+  return 8
+}
+
+func BIT_5_C(this *cpu) int {
+  this.bit(5, this.bc.r_low())
+  return 8
+}
+
+func BIT_5_D(this *cpu) int {
+  this.bit(5, this.de.r_high())
+  return 8
+}
+
+func BIT_5_E(this *cpu) int {
+  this.bit(5, this.de.r_low())
+  return 8
+}
+
+func BIT_5_H(this *cpu) int {
+  this.bit(5, this.hl.r_high())
+  return 8
+}
+
+func BIT_5_L(this *cpu) int {
+  this.bit(5, this.hl.r_low())
+  return 8
+}
+
+func BIT_5_HL(this *cpu) int {
+  this.bit(5, this.mu.Read_8(this.hl.value))
+  return 16
+}
+
+func BIT_5_A(this *cpu) int {
+  this.bit(5, this.af.r_high())
+  return 8
+}
+
+func BIT_4_B(this *cpu) int {
+  this.bit(4, this.bc.r_high())
+  return 8
+}
+
+func BIT_4_C(this *cpu) int {
+  this.bit(4, this.bc.r_low())
+  return 8
+}
+
+func BIT_4_D(this *cpu) int {
+  this.bit(4, this.de.r_high())
+  return 8
+}
+
+func BIT_4_E(this *cpu) int {
+  this.bit(4, this.de.r_low())
+  return 8
+}
+
+func BIT_4_H(this *cpu) int {
+  this.bit(4, this.hl.r_high())
+  return 8
+}
+
+func BIT_4_L(this *cpu) int {
+  this.bit(4, this.hl.r_low())
+  return 8
+}
+
+func BIT_4_A(this *cpu) int {
+  this.bit(4, this.af.r_high())
+  return 8
+}
+
+func BIT_4_HL(this *cpu) int {
+  this.bit(4, this.mu.Read_8(this.hl.value))
+  return 16
+}
+
+func BIT_2_B(this *cpu) int {
+  this.bit(2, this.bc.r_high())
+  return 8
+}
+
+func BIT_2_C(this *cpu) int {
+  this.bit(2, this.bc.r_low())
+  return 8
+}
+
+func BIT_2_D(this *cpu) int {
+  this.bit(2, this.de.r_high())
+  return 8
+}
+
+func BIT_2_E(this *cpu) int {
+  this.bit(2, this.de.r_low())
+  return 8
+}
+
+func BIT_2_H(this *cpu) int {
+  this.bit(2, this.hl.r_high())
+  return 8
+}
+
+func BIT_2_L(this *cpu) int {
+  this.bit(2, this.hl.r_low())
+  return 8
+}
+
+func BIT_2_A(this *cpu) int {
+  this.bit(2, this.af.r_high())
+  return 8
+}
+
+func BIT_2_HL(this *cpu) int {
+  this.bit(2, this.mu.Read_8(this.hl.value))
+  return 16
+}
+
+func BIT_6_B(this *cpu) int {
+  this.bit(6, (*this).bc.r_high())
   return 8
 }
 
@@ -217,8 +407,84 @@ func (this *cpu) do_cb_op(op uint8) int {
     return SRL_HL(this)
   case 0x3f:
     return SRL_A(this)
+  case 0x50:
+    return BIT_2_B(this)
+  case 0x51:
+    return BIT_2_C(this)
+  case 0x52:
+    return BIT_2_D(this)
+  case 0x53:
+    return BIT_2_E(this)
+  case 0x54:
+    return BIT_2_H(this)
+  case 0x55:
+    return BIT_2_L(this)
+  case 0x56:
+    return BIT_2_HL(this)
+  case 0x57:
+    return BIT_2_A(this)
+  case 0x58:
+    return BIT_3_B(this)
+  case 0x59:
+    return BIT_3_C(this)
+  case 0x5a:
+    return BIT_3_D(this)
+  case 0x5b:
+    return BIT_3_E(this)
+  case 0x5c:
+    return BIT_3_H(this)
+  case 0x5d:
+    return BIT_3_L(this)
+  case 0x5e:
+    return BIT_3_HL(this)
+  case 0x5f:
+    return BIT_3_A(this)
+  case 0x60:
+    return BIT_4_B(this)
+  case 0x61:
+    return BIT_4_C(this)
+  case 0x62:
+    return BIT_4_D(this)
+  case 0x63:
+    return BIT_4_E(this)
+  case 0x64:
+    return BIT_4_H(this)
+  case 0x65:
+    return BIT_4_L(this)
+  case 0x66:
+    return BIT_4_HL(this)
+  case 0x67:
+    return BIT_4_A(this)
+  case 0x68:
+    return BIT_5_B(this)
+  case 0x69:
+    return BIT_5_C(this)
+  case 0x6a:
+    return BIT_5_D(this)
+  case 0x6b:
+    return BIT_5_E(this)
+  case 0x6c:
+    return BIT_5_H(this)
+  case 0x6d:
+    return BIT_5_L(this)
+  case 0x6e:
+    return BIT_5_HL(this)
+  case 0x6f:
+    return BIT_5_A(this)
+  case 0x78:
+    return BIT_7_B(this)
+  case 0x79:
+    return BIT_7_C(this)
+  case 0x7a:
+    return BIT_7_D(this)
+  case 0x7b:
+    return BIT_7_E(this)
   case 0x7c:
     return BIT_7_H(this)
+  case 0x7d:
+    return BIT_7_L(this)
+  case 0x7e:
+    return BIT_7_HL(this)
   case 0x7f:
     return BIT_7_A(this)
   case 0x86:
