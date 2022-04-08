6019d35dcbfd3859ea4026f5acb43d7b06722da5
12willi34 <williweissenborn@gmail.com>
2022-04-08 23:01:36 +0200

    calc when read

--- src/memory.go
+++ src/memory.go
@@ -12,42 +12,6 @@ func NewMemoryUnit(boot [0x100]byte, rom []byte) memoryunit {
 		addr: make([]uint8, 0x10000),
     Io: &io,
 	}
-  /*
-  mu.addr[0x04] = 0x1e
-  mu.addr[0x05] = 0x00
-  mu.addr[0x06] = 0x00
-  mu.addr[0x07] = 0xf8
-  mu.addr[0x0F] = 0xe1
-  mu.addr[0x10] = 0x80
-  mu.addr[0x11] = 0xbf
-  mu.addr[0x12] = 0xf3
-  mu.addr[0x14] = 0xbf
-  mu.addr[0x16] = 0x3f
-  mu.addr[0x17] = 0x00
-  mu.addr[0x19] = 0xbf
-  mu.addr[0x1a] = 0x7f
-  mu.addr[0x1b] = 0xff
-  mu.addr[0x1c] = 0x9f
-  mu.addr[0x1e] = 0xbf
-  mu.addr[0x20] = 0xff
-  mu.addr[0x21] = 0x00
-  mu.addr[0x22] = 0x00
-  mu.addr[0x23] = 0xbf
-  mu.addr[0x24] = 0x77
-  mu.addr[0x25] = 0xf3
-  mu.addr[0x26] = 0xf1
-  mu.addr[0x40] = 0x91
-  mu.addr[0x41] = 0x85
-  mu.addr[0x42] = 0x00
-  mu.addr[0x43] = 0x00
-  mu.addr[0x45] = 0x00
-  mu.addr[0x47] = 0xfc
-  mu.addr[0x48] = 0xff
-  mu.addr[0x49] = 0xff
-  mu.addr[0x4a] = 0x00
-  mu.addr[0x4b] = 0x00
-  mu.addr[0xff] = 0x00
-  */
   for i := 0; i < 0x8000 && i < len(rom); i++ {
     mu.addr[i] = rom[i]
   }
@@ -83,7 +47,6 @@ func (this *memoryunit) Write_8(i uint16, data uint8) {
     return
   } else if(i == 0xff00) {
     this.Io.ChangeMode(data)
-    this.addr[0xff00] = this.Io.Get()
     return
   } else if(i == 0xff04) {
     this.addr[0xff04] = 0
@@ -110,7 +73,7 @@ func (this *memoryunit) Write_16(i uint16, data uint16) {
 func (this *memoryunit) read_io(i uint16) uint8 {
   switch i {
   case 0xff00:
-    return this.addr[i]
+    return this.Io.Get()
   case 0xff02:
     return 0xff
   case 0xff10:
@@ -172,5 +135,4 @@ func (this *memoryunit) dma(data uint8) {
 
 func (this *memoryunit) SetBtn(keyCode int, state bool) {
   this.Io.Set(keyCode, state)
-  this.addr[0xff00] = this.Io.Get()
 }
